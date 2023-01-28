016abc3b109fae03567dc1d384a32f286546f709
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-01-28 09:45:51 +0100

    add nav mobile responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const hamburger = document.getElementById("mobile-menu");
-console.log(hamburger);
+const mobileMenu = document.querySelector(".menu");
+
 hamburger.addEventListener("click", function () {
-  const mobileMenu = document.querySelector(".menu");
-  mobileMenu.style.display = "block";
-  console.log(mobileMenu);
+  mobileMenu.classList.toggle("show");
 });
+
+const menuItems = document.querySelectorAll(".menu-items");
+for (let menu of menuItems) {
+  menu.addEventListener("click", function () {
+    if (mobileMenu.classList.contains("show")) {
+      mobileMenu.classList.remove("show");
+    }
+  });
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches(".hamburger") && !e.target.matches(".menu-list")) {
+    const parentDiv = document.getElementsByClassName("menu");
+    for (let i = 0; i < parentDiv.length; i++) {
+      const items = parentDiv[i];
+      if (items.classList.contains("show")) {
+        items.classList.remove("show");
+      }
+    }
+  }
+};
