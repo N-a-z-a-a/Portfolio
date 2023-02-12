5b79d380abeac9eab93df408b86d0a305f45fb93
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-02-12 05:46:13 +0100

    refactor skills section and add assets folder

--- script.js
+++ script.js
@@ -25,3 +25,64 @@ window.onclick = function (e) {
     }
   }
 };
+
+const form = document.getElementById("form");
+const nameField = document.getElementById("name");
+const error = document.querySelector(".error");
+console.log(error);
+
+nameField.addEventListener("input", (e) => {
+  if (nameField.validity.valid) {
+    error.textContent = "";
+  } else {
+    showError();
+  }
+});
+
+function showError() {
+  if (nameField.validity.valueMissing) {
+    console.log(nameField.validity);
+    error.textContent = "You need to enter your full name.";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  if (!nameField.validity.valid) {
+    showError();
+    preventDefault();
+  }
+});
+
+email.addEventListener("input", (event) => {
+  // Each time the user types something, we check if the
+  // form fields are valid.
+
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+
+  // Set the styling appropriately
+  emailError.className = "error active";
+}
